6d3f836f55f29723b51e78f899900780c07a77d5
Brandon Dong <gasingdong@gmail.com>
2019-09-19 13:53:41 -0700

    write two tests for post endpoint

--- api/server.spec.ts
+++ api/server.spec.ts
@@ -7,7 +7,7 @@ beforeAll(async () => {
   await db.migrate.latest();
 });
 
-describe('messages model', () => {
+describe('server.js', () => {
   beforeEach(async () => {
     await db('messages').truncate();
   });
@@ -16,13 +16,33 @@ describe('messages model', () => {
     expect(process.env.DB_ENV).toBe('testing');
   });
 
-  it('should return 200 OK', async () => {
-    const res = await request(server).get('/api/messages');
-    expect(res.status).toBe(200);
+  describe('GET MESSAGES', () => {
+    it('should return 200 OK', async () => {
+      const res = await request(server).get('/api/messages');
+      expect(res.status).toBe(200);
+    });
+
+    it('should return JSON', async () => {
+      const res = await request(server).get('/api/messages');
+      expect(res.type).toMatch(/json/i);
+    });
   });
 
-  it('should return JSON', async () => {
-    const res = await request(server).get('/api/messages');
-    expect(res.type).toMatch(/json/i);
+  describe('POST MESSAGES', () => {
+    it('should post messages', async () => {
+      await request(server)
+        .post('/api/messages')
+        .send({ message: 'Hello world!' });
+      const results = await db('messages');
+      expect(results).toHaveLength(1);
+    });
+
+    it('should return matching id', async () => {
+      const res = await request(server)
+        .post('/api/messages')
+        .send({ message: 'Hello world!' });
+      const [id] = res.body;
+      expect(id).toBe(1);
+    });
   });
 });
